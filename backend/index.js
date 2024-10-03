d42c841b4e797290a18c26d1a4d42bbbf08cbe08
Samir Maity <91019169+samir1maity@users.noreply.github.com>
2024-10-03 09:34:22 +0530

    done basic db setup

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,15 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import jwt from "jsonwebtoken";
-// require("dotenv").config();
 import dotenv from "dotenv";
 dotenv.config();
+import mongoose from "mongoose";
+
+import users from "./src/config/db.js";
 
 const JWT_SECRET = process.env.JWT_SECRET;
+
+mongoose.connect(process.env.DATABASE_URL);
 
 const app = express();
 app.use(express.json());
@@ -19,6 +23,28 @@ app.use(
     origin: "http://localhost:5173",
   })
 );
+
+app.post("/signup", async (req, res) => {
+  const name = "samir";
+  const email = req.body.email;
+  const password = req.body.password;
+  const username = req.body.username;
+
+  console.log("req.body.email", req.body.email);
+
+  const temp = await users.create({
+    name: name,
+    email: email,
+    password: password,
+    username: username,
+  });
+
+  console.log("temp", temp);
+
+  res.json({
+    msg: "register success",
+  });
+});
 
 app.post("/signin", (req, res) => {
   const email = req.body.email;
---
+++ backend/src/config/db.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const userModel = new Schema({
+  name: String,
+  email: { type: String },
+  password: String,
+  username: { type: String },
+});
+
+export default mongoose.model("users", userModel);
